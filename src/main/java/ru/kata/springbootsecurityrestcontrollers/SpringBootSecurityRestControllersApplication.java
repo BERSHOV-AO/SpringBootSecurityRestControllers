73883008b016376c33c28f16946c2874e524e5f0
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-19 20:58:37 +0300

    Initial commit

---
+++ src/main/java/ru/kata/springbootsecurityrestcontrollers/SpringBootSecurityRestControllersApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.springbootsecurityrestcontrollers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRestControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityRestControllersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kata/springbootsecurityrestcontrollers/SpringBootSecurityRestControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.springbootsecurityrestcontrollers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityRestControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
